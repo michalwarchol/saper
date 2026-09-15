5a1352610ba6d1f80ef4e6e92fdebd4486d54ce8
michalwarchol <imsimilar@gmail.com>
2026-09-15 12:48:26 +0200

    fix(ui): Correct update remaining mines

--- src/ui/Game/Game.tsx
+++ src/ui/Game/Game.tsx
@@ -3,22 +3,29 @@ import plansze from "../../../assets/saper-plansze.json";
 
 import { createBoard, toggleFlag, revealCell, type Board as TBoard, type Level as TLevel } from "../../logic/board";
 import Board from "../Board";
-import MenuUI from "../Menu";
+import Menu from "../Menu";
 
 const Game = () => {
   const [currentLevelId, setCurrentLevelId] = useState(plansze.levels[0].id);
   const [board, setBoard] = useState<TBoard>(createBoard(plansze.levels[0] as TLevel));
-  const [remainedMines, setRemainedMines] = useState(plansze.levels[0].mineCount);
+  const [remainedMines, setRemainedMines] = useState(board.cells.filter((cell) => cell.mine).length);
 
   const onSetLevel = (id: string) => {
     const newLevel = plansze.levels.find((level) => level.id === id);
-    setBoard(createBoard(newLevel as TLevel));
-    setRemainedMines(newLevel!.mineCount!);
+    const newBoard = createBoard(newLevel as TLevel);
+    setBoard(newBoard);
+    // important: mineCount from json file is omitted
+    setRemainedMines(newBoard.cells.filter((cell) => cell.mine).length);
     setCurrentLevelId(newLevel!.id!);
   }
 
   const onToggleFlag = (index: number) => {
-    setBoard(toggleFlag(board, index));
+    const newBoard = toggleFlag(board, index);
+    const allMines = newBoard.cells.filter((cell) => cell.mine).length
+    const flags = newBoard.cells.filter((cell) => cell.flagged).length;
+
+    setBoard(newBoard);
+    setRemainedMines(allMines - flags);
   }
 
   const onRevealCell = (index: number) => {
@@ -27,7 +34,7 @@ const Game = () => {
 
   return (
     <div>
-      <MenuUI
+      <Menu
         currentLevelId={currentLevelId}
         levels={plansze.levels.map((level) => ({ id: level.id, name: level.name}))}
         onSetLevel={onSetLevel}
